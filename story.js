f65bab9a66e5afbb104823f43581fabebe662deb
Fatih Erikli <fatiherikli@gmail.com>
2016-06-26 10:42:28 +0300

    Initial commit

---
+++ story.js
@@ -0,0 +1,29 @@
+(function (root) {
+  root.__STORY__ = {
+    'SLEEP': 'Kahramanımız uyuyor.',
+    'WAKE_UP': 'Uyandı.',
+    'BREAKFAST': 'Kahvaltı yapıyor.',
+    'LEAVE_HOME': 'Evden çıkıyor.',
+    'START_ENGINE': 'Arabayı çalıştıracak.',
+    'WONT_START': 'Arabayı çalışmadı.',
+    'TRY_AGAIN': 'Çalıştırmayı tekrar deniyor.',
+    'DRIVE_CAR': 'Araba sürüyor.',
+    'RIDE_BIKE': 'Bisiklet sürüyor.',
+    'REACH_OFFICE': 'Ofise ulaştı.',
+    'WORK': 'Çalışıyor.',
+    'LEAVE_OFFICE': 'Ofisten çıkıyor.',
+    'TURN_HOME': 'Eve döndü.',
+    'HIT_BY_HORSE': 'At çarptı.',
+    'HIT_BY_TRUCK': 'Kamyon çarptı.',
+    'WAIT_AMBULANCE': 'Ambulans bekliyor.',
+    'REACH_HOSPITAL': 'Hastaneye ulaştı.',
+    'DIE': 'Kahramanımız öldü.',
+    'GET_WELL': 'Kahramanımız iyileşti.',
+    'GOD_EXISTS': 'Allah var',
+    'GO_TO_HELL': 'Cehenneme gitti',
+    'GO_TO_HEAVEN': 'Cennete gitti',
+    'GO_TO_HEAVEN': 'Cennete gitti',
+    'REINCARNATE_AS_TRUCK': 'Kahramanımız bir kamyon olarak reenkarne oldu',
+    'REINCARNATE_AS_CABBAGE': 'Kahramanımız bir lahana olarak reenkarne oldu'
+  };
+})(this);
